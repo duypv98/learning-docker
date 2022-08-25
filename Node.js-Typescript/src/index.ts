53f6f7986f4b028d79e8941a5805544aca4549b8
duypv <duypv98@gmail.com>
2022-08-25 22:29:23 +0700

    feat: dockerfile & expose with env

--- Node.js-Typescript/src/index.ts
+++ Node.js-Typescript/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import dotenv from "./utils/dotenv";
+dotenv.config();
 
-const PORT = 8080;
+const PORT = Number(process.env.PORT || 8080);
 const HOST = "0.0.0.0";
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(`Hello World: ${process.env.MESSAGE || ""}`);
 });
 
 app.listen(PORT, HOST);
